505ca0169f21c175c2efa871d23263e51e879145
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2023-04-06 15:29:29 +0100

    initial setup

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={() => { setCount(count + 1) }} title="press"
+      />
+      <View style={styles.Button}>
+        <Button
+          onPress={() => { setCount(0) }} title="Reset"
+        />
+        <Text>You have pressed {count} times</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Button: {
+    marginVertical: 2,
+    marginBottom: 2
+  }
 });
